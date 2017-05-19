20ebdf3f8432724734a0a5bdc3dcf2446cf0bc9e
awsh10 <awsh@ukr.net>
2017-05-20 00:33:49 +0300

    create models

--- probability/models.py
+++ probability/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Experiment(models.Model):
+    #class Meta():
+    date = models.DateTimeField(default=timezone.now, unique=True)
+    count = models.IntegerField(default=0)
+
+
+class ExperimentData(models.Model):
+    experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
+    date_ms = models.IntegerField(default=0)
+    relative_date_ms = models.IntegerField(default=0)
